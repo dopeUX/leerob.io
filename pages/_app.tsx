80165a6d1dc4c367d8eee77ee8884e66ef8e625c
UXNation <uxnation.inc@gmail.com>
2022-07-30 11:22:21 +0530

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { useAnalytics } from 'lib/analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
   useAnalytics();
-
+  ///efuehwfguiew
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
